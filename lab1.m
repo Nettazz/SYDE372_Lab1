d5acea8de05d07e8457125bea3227e161e4fd542
Thomas Fortin <t2fortin@uwaterloo.ca>
2020-02-01 10:18:24 -0500

    created distrubutions

---
+++ lab1.m
@@ -0,0 +1,47 @@
+clear
+
+
+Class_A = data(200, [5; 10], [8, 0; 0, 4]);
+Class_B = data(200, [10; 15], [8, 0; 0, 4]);
+Class_C = data(100, [5; 10], [8, 4; 4, 40;]);
+Class_D = data(200, [15; 10], [8, 0; 0, 8]);
+Class_E = data(150, [10; 5], [10, -5; -5, 20]);
+
+Cont_A = std_cont([5; 10], [8, 0; 0, 4]);
+Cont_B = std_cont([10; 15], [8, 0; 0, 4]);
+Cont_C = std_cont([5; 10], [8, 4; 4, 40;]);
+Cont_D = std_cont([15; 10], [8, 0; 0, 8]);
+Cont_E = std_cont([10; 5], [10, -5; -5, 20]);
+
+figure(1)
+hold on
+scatter(Class_A(1, :), Class_A(2, :),'x', 'red');
+plot(Cont_A(1, :), Cont_A(2, :), 'red', 'LineWidth', 2);
+scatter(Class_B(1, :), Class_B(2, :), 'x', 'blue');
+plot(Cont_B(1, :), Cont_B(2, :), 'blue', 'LineWidth', 2);
+hold off
+
+figure(2)
+hold on
+scatter(Class_C(1, :), Class_C(2, :), 'x', 'red');
+plot(Cont_C(1, :), Cont_C(2, :), 'red', 'LineWidth', 2);
+scatter(Class_D(1, :), Class_D(2, :), 'x', 'blue');
+plot(Cont_D(1, :), Cont_D(2, :), 'blue', 'LineWidth', 2);
+scatter(Class_E(1, :), Class_E(2, :), 'x', 'green');
+plot(Cont_E(1, :), Cont_E(2, :), 'green', 'LineWidth', 2);
+hold off
+
+
+function Class = data(n, u, cov)
+   x = randn(2, n);
+   x = cov * x;
+   Class = bsxfun(@plus, x, u);
+end
+
+function contour = std_cont(u, cov)
+    x = linspace(0, 2 * pi);
+    unit_centered_contour = [cos(x); sin(x)];
+    centered_contour = cov * unit_centered_contour;
+    contour = bsxfun(@plus, centered_contour, u);
+end
+
